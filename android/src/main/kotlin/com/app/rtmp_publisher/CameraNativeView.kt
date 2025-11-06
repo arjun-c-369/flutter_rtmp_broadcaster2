4d0501dee709abe0900c27ea027fc9f9363b8396
arjun-c-369 <62787419+arjun-c-369@users.noreply.github.com>
2025-11-06 16:57:27 +0530

    Update CameraNativeView.kt

--- android/src/main/kotlin/com/app/rtmp_publisher/CameraNativeView.kt
+++ android/src/main/kotlin/com/app/rtmp_publisher/CameraNativeView.kt
@@ -209,8 +209,8 @@ class CameraNativeView(
                 val size = streamingSize["size"] as Size
                 val bitrateRes = streamingSize["bitrate"] as Int
                 if (rtmpCamera.isRecording || rtmpCamera.prepareAudio() && rtmpCamera.prepareVideo(
-                        (size.width*0.2).toInt(),
-                        (size.height*0.2).toInt(),
+                        (size.width*0.6).toInt(),
+                        (size.height*0.6).toInt(),
                         bitrate ?: bitrateRes
                     )
                 ) {
